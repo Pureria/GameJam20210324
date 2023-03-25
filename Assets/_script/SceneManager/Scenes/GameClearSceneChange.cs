e5d91fcdf21cfaacd3f26f4cc913c88fe40c9d8f
Pureria <117608289+Pureria@users.noreply.github.com>
2023-03-25 15:27:53 +0900

    audio

--- Assets/_script/SceneManager/Scenes/GameClearSceneChange.cs
+++ Assets/_script/SceneManager/Scenes/GameClearSceneChange.cs
@@ -4,8 +4,30 @@ using UnityEngine;
 
 public class GameClearSceneChange : SceneChange
 {
+    [SerializeField] private AudioSource audio;
+    bool flg;
+
+    private void Start()
+    {
+        flg = false;
+    }
+
+    private void Update()
+    {
+        if (flg && !audio.isPlaying)
+        {
+            base.ClickSceneChange();
+
+            Player.gameStart = false;
+            Player.goal = false;
+            Player.dead = false;
+        }
+    }
     public void GameClearNextSceneChange()
     {
-        base.ClickSceneChange();
+        if (audio.isPlaying)
+        {
+            flg = true;
+        }
     }
 }
--- Assets/_script/SceneManager/Scenes/GameOverSceneChange.cs
+++ Assets/_script/SceneManager/Scenes/GameOverSceneChange.cs
@@ -1,11 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameOverSceneChange : SceneChange
 {
+    [SerializeField] private AudioSource audio;
+    bool flg;
+    private void Start()
+    {
+        flg = false;
+    }
+
+    private void Update()
+    {
+        if(flg && !audio.isPlaying)
+        {
+            base.ClickSceneChange();
+
+            Player.gameStart = false;
+            Player.goal = false;
+            Player.dead = false;
+        }
+    }
+
     public void GameOverNextSceneChange()
     {
-        base.ClickSceneChange();
+        if (audio.isPlaying)
+        {
+            flg = true;
+        }
+
     }
 }
